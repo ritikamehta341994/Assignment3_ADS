8c70fb7d0ea3cdd000631a8f3c8d0d5f8fe6bf9e
ritika <ritzm03@gmail.com>
2022-04-02 00:56:52 +0100

    Final commit for ADS Assignment 3

--- Heaps/src/CrimeIncidents.java
+++ Heaps/src/CrimeIncidents.java
@@ -5,12 +5,16 @@
  */
 public class CrimeIncidents {
 	
-	private Long incidentId;
-	private String caseNumber;
-	private String incidentType;
-	private String address;
-	private String dayOfWeek;
-	private int zipCode;
+	private Long incidentId; // to store the incident Id of crime
+	private String caseNumber; // to store the case number of crime
+	private String incidentType; // to store the incident type of crime
+	private String address; // to store the address of where the crime was committed
+	private String dayOfWeek; // to store day of the week when crime was committed
+	private int zipCode; // to store zip code of where the crime was committed
+	
+	/*
+	 * constructor for crimeIncidents
+	 */
 	public CrimeIncidents(Long incidentId, String caseNumber, String incidentType, String address, String dayOfWeek,
 			int zipCode) {
 		
@@ -21,39 +25,80 @@ public class CrimeIncidents {
 		this.dayOfWeek = dayOfWeek;
 		this.zipCode = zipCode;
 	}
+	
+	/**
+	 * returns the incidentId
+	 * @return incident Id
+	 */
 	public Long getIncidentId() {
 		return incidentId;
 	}
+	/**
+	 * sets the incidentId
+	 * @param incidentId
+	 */
 	public void setIncidentId(Long incidentId) {
 		this.incidentId = incidentId;
 	}
+	/*
+	 *  returns the caseNumber
+	 *  
+	 */
 	public String getCaseNumber() {
 		return caseNumber;
 	}
+	
+	/*
+	 * sets the caseNumber
+	 */
 	public void setCaseNumber(String caseNumber) {
 		this.caseNumber = caseNumber;
 	}
+	/*
+	 *  returns incidentType
+	 */
 	public String getIncidentType() {
 		return incidentType;
 	}
+	/*
+	 *  sets incidentType
+	 */
 	public void setIncidentType(String incidentType) {
 		this.incidentType = incidentType;
 	}
+	/*
+	 *  returns address
+	 */
 	public String getAddress() {
 		return address;
 	}
+	/*
+	 *  sets address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	/*
+	 *  return dayOfWeek
+	 */
 	public String getDayOfWeek() {
 		return dayOfWeek;
 	}
+	/*
+	 *  sets dayOfWeek
+	 */
 	public void setDayOfWeek(String dayOfWeek) {
 		this.dayOfWeek = dayOfWeek;
 	}
+	/*
+	 *  returns zipCode
+	 */
 	public int getZipCode() {
 		return zipCode;
 	}
+	/*
+	 *  sets zipCodeS
+	 */
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
--- Heaps/src/CrimeIncidentsData.java
+++ Heaps/src/CrimeIncidentsData.java
@@ -1,27 +1,46 @@
 import java.util.LinkedList;
+/*
+ * This class stores the count of crime incidents and the crime incidents for a particular zip code
+ */
+
 public class CrimeIncidentsData {
 
-	private int count;
-	private LinkedList<CrimeIncidents> crimeIncidents;
+	private int count; //count of crime incidents for the zip code
+	private LinkedList<CrimeIncidents> crimeIncidents; // list of crime incidents for the zip code
 	
+	/*
+	 * Constructor for the class
+	 */
 	public CrimeIncidentsData(int count, LinkedList<CrimeIncidents> crimeIncidents) {
 		super();
 		this.count = count;
 		this.crimeIncidents = crimeIncidents;
 	}
 
+	/*
+	 * returns the count
+	 */
 	public int getCount() {
 		return count;
 	}
 
+	/*
+	 * sets the count
+	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
 
+	/*
+	 * returns the list of crime incidents
+	 */
 	public LinkedList<CrimeIncidents> getCrimeIncidents() {
 		return crimeIncidents;
 	}
 
+	/*
+	 * sets the list of crime incidents
+	 */
 	public void setCrimeIncidents(LinkedList<CrimeIncidents> crimeIncidents) {
 		this.crimeIncidents = crimeIncidents;
 	}
